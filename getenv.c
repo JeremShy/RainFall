adf34994717c405dd3b12f7572980654ee94cf75
Jeremy CAMHI <jcamhi@student.42.fr>
2018-11-22 14:55:24 +0100

    fini

--- getenv.c
+++
@@ -1,7 +0,0 @@
-int main (int argc, char *argv[]) {
-   if (argc < 3)
-      printf ("Usage : %s <variable environnement> <executable>\n", argv[0]);
-   else
-      printf ("%s is located at %p\n", argv[1], getenv(argv[1]) + (strlen(argv[0]) - strlen(argv[2]))*2);
-   return 0;
-}
